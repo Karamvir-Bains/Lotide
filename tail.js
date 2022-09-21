23be7656195af999be0973affdebb6fd41c97e41
Karamvir-Bains <kbains2000@gmail.com>
2022-09-21 17:22:59 -0600

    Tail function added

--- tail.js
+++ tail.js
@@ -8,10 +8,11 @@ const assertEqual = function(actual, expected) {
 };
 
 // FUNCTION IMPLEMENTATION
-const head = function(arr) {
-  return arr[0];
+const tail = function(arr) {
+  return arr.slice(1);
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
