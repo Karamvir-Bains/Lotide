9d20499270305bbf34dfdc2238410c55f99189c2
Karamvir-Bains <kbains2000@gmail.com>
2022-09-21 16:45:48 -0600

    head function added

--- head.js
+++ head.js
@@ -7,6 +7,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// FUNCTION IMPLEMENTATION
+const head = function(arr){
+  return arr[0];
+};
+
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
