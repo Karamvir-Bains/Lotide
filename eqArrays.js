0039b53bae6db562a2e1d9f5ff9a0323fc381cf6
Karamvir-Bains <kbains2000@gmail.com>
2022-09-23 19:20:58 -0600

    Added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  for (let i in arr1) {
+    if (arr1[i] !== arr2[i]) {
+      return assertEqual(false, true);
+    }
+  }
+  return assertEqual(true, true);
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
