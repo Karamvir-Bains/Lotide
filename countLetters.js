52d4f0a402fdbb260679ca522e44dcfdc9111b7a
Karamvir-Bains <kbains2000@gmail.com>
2022-09-26 13:32:39 -0600

    Added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+// Takes in 2 values to determine if they match, and console logs if they match or if they don't
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Takes in a string and returns a object with an count of each letter in the string
+const countLetters = function(sentence) {
+  let results = {};
+  for (let letter of sentence) {
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+// Test Code
+console.log(countLetters("lighthouse in the house"));
