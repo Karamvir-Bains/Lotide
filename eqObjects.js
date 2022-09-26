add571b75692172a5ba315c347c9555927b2aa3f
Karamvir-Bains <kbains2000@gmail.com>
2022-09-26 16:19:08 -0600

    Added eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -7,12 +7,29 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// Takes in 2 arrays and returns true if they match or false if they don't
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i in arr1) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 // Returns true if both objects have identical keys with identical values
 // Otherwise false.
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) return false;
   for (let key of Object.keys(object1)) {
-    if (object2[key] === undefined) {
+    let keyValue1 = object1[key];
+    let keyValue2 = object2[key];
+    if (Array.isArray(keyValue1) && Array.isArray(keyValue2)) {
+      if (!eqArrays(keyValue1, keyValue2)) {
+        return false;
+      }
+    } else if (keyValue1 !== keyValue2) {
       return false;
     }
   }
@@ -24,8 +41,13 @@ const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const abc = { a: "1", b: "2", c: "3" };
 const adc = { a: "1", d: "2", c: "3" };
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
 
 // Test Code
 assertEqual(eqObjects(ab, ba), true);
 assertEqual(eqObjects(ab, abc), false);
 assertEqual(eqObjects(abc, adc), false);
+assertEqual(eqObjects(cd, dc), true);
+assertEqual(eqObjects(cd, cd2), false);
