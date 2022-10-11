7849ef2f829757257ac515e2bafb54920e7c8659
Karamvir-Bains <kbains2000@gmail.com>
2022-10-11 15:08:33 -0600

    Refactored tail.js

--- tail.js
+++ tail.js
@@ -1,18 +1,6 @@
-// Takes in 2 values to determine if they match, and console logs if they match or if they don't
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // Takes in an array and returns everything but the first element in the array
 const tail = function(arr) {
   return arr.slice(1);
 };
 
-// Test Code
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
