073351483909b7518b8b75ffb3cbefda8ef7f991
Karamvir-Bains <kbains2000@gmail.com>
2022-10-15 13:16:28 -0600

    Added all funcitons to index.js

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const head   = require('./head');
 const tail   = require('./tail');
+const eqArrays = require('./eqArrays');
+const without = require('./without');
+const flatten = require('./flatten');
 const middle = require('./middle');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
 const eqObjects = require('./eqObjects');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const findKey = require('./findKey');
 
 module.exports = {
   head,
   tail,
+  eqArrays,
+  without,
+  flatten,
   middle,
-  eqObjects
+  countOnly,
+  countLetters,
+  letterPositions,
+  findKeyByValue,
+  eqObjects,
+  map,
+  takeUntil,
+  findKey
 };
