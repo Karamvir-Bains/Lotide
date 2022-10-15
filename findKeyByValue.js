66504e7ba50e09638d9cd775270e5e1baef28056
Karamvir-Bains <kbains2000@gmail.com>
2022-10-15 12:59:51 -0600

    Modularized and added testing with Mocha and Chai for findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,13 +1,3 @@
-// Takes in 2 values to determine if they match, and console logs if they match or if they don't
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 // Takes in an object and a value, if the value is associated with a key it returns the key, otherwise undefined
 const findKeyByValue = function(object, value) {
   for (let key in object) {
@@ -18,12 +8,4 @@ const findKeyByValue = function(object, value) {
   return undefined;
 };
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-// Test Code
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
